ca80b96ea5def800e7f6080eeca611adbb143038
qusai als <qosai.web@gmail.com>
2026-05-14 00:53:58 +0200

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -3,12 +3,8 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   images: { unoptimized: true },
   optimizeFonts: false,
-  trailingSlash: true,
 };
 
 module.exports = nextConfig;
